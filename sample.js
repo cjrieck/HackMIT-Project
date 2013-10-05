4b6d1d52519c928db775e9f70613cc005ba84ba4
Bryan <Funstorm17@gmail.com>
2013-10-05 19:29:33 -0400

    Testing stuff

--- sample.js
+++ sample.js
@@ -7,12 +7,15 @@ var app = http.createServer(function(request, response) {
 	var pathname = url.parse(request.url).pathname;
 
 	fs.readFile('./simplePage.html', function(err, html) {
+		if (err) {
+			throw err;
+		}
 		fileHTML = html;
 	});
 	
 	response.writeHead(200, {"Content-Type": "text/html"});
-	// response.write("Hello World at " + pathname);
-	response.write(typeof(fileHTML));
+	response.write("Hello World at " + pathname);
+	// response.write(typeof(fileHTML));
 	response.end();
 });
 
