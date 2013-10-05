7b5cfb986c08e66a92bc5e9a1631238a638543e1
Clayton Rieck <cjrieck123@gmail.com>
2013-10-05 19:10:53 -0400

    Trying something new

--- sample.js
+++ sample.js
@@ -1,9 +1,17 @@
 var http = require("http");
+var fs = require("fs");
 
 var app = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "html"});
-  response.write("simplePage.html");
-  response.end();
+
+  fs.readfile('./simplePage.html', function(err, html){
+  	if (err){
+  		throw err;
+  	}
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("simplePage.html");
+    response.end();
+  });
+  
 });
 
 var port = process.env.PORT || 5000;
