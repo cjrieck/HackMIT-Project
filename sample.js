e85d1fcb2d2dc194d4c4210914b42afc6dac6f62
Bryan <Funstorm17@gmail.com>
2013-10-05 19:22:53 -0400

    Testing stuff

--- sample.js
+++ sample.js
@@ -3,12 +3,12 @@ var fs = require("fs");
 var url = require("url");
 
 var app = http.createServer(function(request, response) {
-	// var fileHTML;
+	var fileHTML;
 	var pathname = url.parse(request.url).pathname;
 
-	// fs.readFile('./simplePage.html', function(err, html) {
-	// 	fileHTML = html;
-	// });
+	fs.readFile('./simplePage.html', function(err, html) {
+		fileHTML = html;
+	});
 	
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World at " + pathname);
