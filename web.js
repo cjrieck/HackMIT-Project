514031313adc68de72f61bbe5d9b241f4757dbe4
Clayton Rieck <cjrieck123@gmail.com>
2013-10-05 17:48:10 -0400

    Added test.html

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('simplePage');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
