979f3ebb67aec4a737d5ffda882de79a1c9d38b8
Bryan <Funstorm17@gmail.com>
2013-10-05 18:51:21 -0400

    small changes

--- sample.js
+++ sample.js
@@ -1,6 +1,8 @@
 var http = require("http");
 
-var app = http.createServer(function(request, response) {
+var app = http.createServer();
+
+app.get("/", function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
