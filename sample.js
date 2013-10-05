78c4b14ba7a5fd203aefb9c73915bf5ed3ee4e27
Bryan <Funstorm17@gmail.com>
2013-10-05 18:53:55 -0400

    reverting

--- sample.js
+++ sample.js
@@ -1,8 +1,6 @@
 var http = require("http");
 
-var app = http.createServer();
-
-app.get("/", function(request, response) {
+var app = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
