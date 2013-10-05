d73e521a74f87662e2cff6a44bfb3df514d6d194
Bryan <Funstorm17@gmail.com>
2013-10-05 19:21:27 -0400

    Testing stuff

--- sample.js
+++ sample.js
@@ -1,26 +1,25 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
 
 var app = http.createServer(function(request, response) {
+	// var fileHTML;
+	var pathname = url.parse(request.url).pathname;
 
-	fs.readfile('./simplePage.html', function(err, html){
-		if (err){
-			throw err;
-		}
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write("simplePage.html");
-		response.end();
-	});
+	// fs.readFile('./simplePage.html', function(err, html) {
+	// 	fileHTML = html;
+	// });
 	
-// var url = require("url");
-
-// var app = http.createServer(function(request, response) {
-// 	var pathname = url.parse(request.url).pathname;
-// 	response.writeHead(200, {"Content-Type": "text/plain"});
-// 	response.write("Hello World at " + pathname);
-// 	response.end();
-
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World at " + pathname);
+	response.end();
+	// response.writeHead(200, {"Content-Type": "text/html"});
+	// response.write(fileHTML);
+	// response.end();
 });
+	
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port);
