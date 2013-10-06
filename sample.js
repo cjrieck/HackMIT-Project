f6421fd45c5ef75efb5e771fec02b2006c35256a
Bryan <Funstorm17@gmail.com>
2013-10-05 23:56:31 -0400

    Small changes

--- sample.js
+++ sample.js
@@ -4,10 +4,34 @@ var url = require("url");
 
 var app = http.createServer(function(request, response) {
 	var pathname = url.parse(request.url).pathname;
+	if (pathname[pathname.length-1] == '/') pathname = pathname.slice(0, pathname.length-1);
+	console.log("\nGot a request at: " + pathname);
 
-	html = fs.readFileSync('./simplePage.html', 'utf-8');
+	if (pathname == '/favicon.ico') {
+		console.log("Got a request for a favicon.");
+		response.end();
+		return;
+	}
+
+	var splitPath = pathname.split('/');
+	if (splitPath.length > 2) {
+		response.write('Hahahaha, you done entered a bad URL.');
+		console.log(splitPath.length);
+		response.end(); // Tells the broswer that the response is complete
+		return;
+	}
+
+	if (pathname == '') {
+		var filename = './simplePage.html';
+	}
+	else {
+		var filename = './test.html';
+	}
+
+	html = fs.readFileSync(filename, 'utf-8');
 	response.write(html);
-	response.end();
+	console.log("Served html (" + filename + ") file to broswer.");
+	response.end(); // Tells the broswer that the response is complete
 });
 
 var port = process.env.PORT || 7500;
