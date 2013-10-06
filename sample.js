36bb0595813db50817bf22ef40eba864a719ba66
Bryan <Funstorm17@gmail.com>
2013-10-05 20:01:48 -0400

    Pulling in Clayton's changes

--- sample.js
+++ sample.js
@@ -3,16 +3,16 @@ var fs = require("fs");
 var url = require("url");
 
 var app = http.createServer(function(request, response) {
+	var fileHTML;
 	var pathname = url.parse(request.url).pathname;
 
 	fs.readFile('./simplePage.html', 'utf-8', function(err, html) {
-		if (err) {
-			throw err;
-		}
-		// response.write("Hello World at " + pathname);
-		response.write(html);
-		response.end();
+		fileHTML = html;
+		response.write(fileHTML);
 	});
+
+	response.write("Hello World at " + pathname + "\n");
+	response.end();
 });
 
 var port = process.env.PORT || 5000;
